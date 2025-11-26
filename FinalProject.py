82464c1abff1c204ff4e5f1e847e49661114f65c
hc0426 <HarshChavva@my.unt.edu>
2025-11-26 13:51:25 -0600

    fixed df.read data reference

--- FinalProject.py
+++ FinalProject.py
@@ -2,5 +2,5 @@ import pandas as pd
 # import matplotlib as plt
 # import seaborn as sns
 
-df = pd.read_csv("C:\Revature_110325\Group2FinalProject\data\StormEvents_details.csv")
+df = pd.read_csv("data\StormEvents_details.csv")
 print(df.head())
