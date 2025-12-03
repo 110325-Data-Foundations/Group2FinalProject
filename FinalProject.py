b14cddc00fdb7aa338abda4fad997df45aff91e5
mxnajam <m.najam1314@gmail.com>
2025-12-02 22:01:27 -0600

    New scripts, triggers, audit_log table, modular load_data script

--- FinalProject.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-# import matplotlib as plt
-# import seaborn as sns
-
-df = pd.read_csv("C:\Revature_110325\Group2FinalProject\data\StormEvents_details.csv")
-print(df.head())
--- scripts/test.sql
+++ scripts/test.sql
@@ -1,4 +1,8 @@
 
-SELECT *
-FROM storm_events_details_invalid
-LIMIT 20;
+-- SELECT *
+-- FROM storm_events_details_invalid
+-- LIMIT 20;
+ --ELECT COUNT(*) FROM storm_events_details;
+-- SELECT COUNT(*) FROM storm_events_details_cleaned;
+-- SELECT COUNT(*) FROM storm_events_details_invalid;
+SELECT * FROM storm_events_audit LIMIT 10;
